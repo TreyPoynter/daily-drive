bdd4a4b0bfc89c32d9e5ad07808f02b4f9cceea2
TreyPoynter <treypoynter@gmail.com>
2024-10-01 09:32:48 -0500

    started my goals screen

--- src/Goals/Goals.tsx
+++ src/Goals/Goals.tsx
@@ -1,24 +1,35 @@
 import { Text, View, StyleSheet, Pressable } from "react-native";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
+import { FontAwesome6 } from "@expo/vector-icons";
+import GradientBackgroundWrapper from "../components/GradientBackgroundWrapper/GradientBackgroundWrapper";
 
 const Goals = () => {
   const nav = useNavigation<NativeStackNavigationProp<any>>();
 
   return (
-    <View style={styles.container}>
-      <Pressable onPress={() => nav.navigate('AddGoalScreen')}>
-        <Text>ADD GOAL</Text>
-      </Pressable>
-    </View>
+    <GradientBackgroundWrapper>
+      <View style={styles.container}>
+        <View style={{flexDirection: 'row',alignItems: 'center'}}>
+          <Pressable onPress={() => nav.navigate('Home')}>
+            <FontAwesome6 name="angle-left" size={24} color="black" />
+          </Pressable>
+          <Text style={{marginLeft: '36.5%', fontFamily: 'Inter-SemiBold'}}>My Goals</Text>
+        </View>
+        <View style={{marginTop: 30}}>
+          <Text style={{fontFamily: 'Inter-SemiBold'}}>Select</Text>
+        </View>
+      </View>
+    </GradientBackgroundWrapper>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start',
+    paddingTop: 50,
+    width: '100%',
+    paddingHorizontal: 20,
   }
 })
 
